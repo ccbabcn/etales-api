171b5206d1b96d3b1cfc336a42917c3b96056358
Cristian <cristian@Cristians-MacBook-Pro.local>
2023-10-05 23:41:14 +0200

    Adds initial password encryption

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, Index } from "typeorm";
+import { Entity, Column, Index, BeforeInsert } from "typeorm";
+import bcrypt from "bcryptjs";
 import Model from "./model.entity";
 
 export enum RoleEnumType {
@@ -36,6 +37,20 @@ export class User extends Model {
     default: false,
   })
   verified: boolean;
+
+  // ? Hash password before saving to database
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  // ? Validate password
+  static async comparePasswords(
+    candidatePassword: string,
+    hashedPassword: string
+  ) {
+    return await bcrypt.compare(candidatePassword, hashedPassword);
+  }
 
   toJSON() {
     return { ...this, password: undefined, verified: undefined };
