bd754d6a3130c8ad9d38754554e7ec291585f110
Cristian <cristian@Cristians-MacBook-Pro.local>
2023-10-10 23:30:09 +0200

    Adds logout method to auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -161,3 +161,28 @@ export const refreshAccessTokenHandler = async (
     next(err);
   }
 };
+
+const logout = (res: Response) => {
+  res.cookie("access_token", "", { maxAge: -1 });
+  res.cookie("refresh_token", "", { maxAge: -1 });
+  res.cookie("logged_in", "", { maxAge: -1 });
+};
+
+export const logoutHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = res.locals.user;
+
+    await redisClient.del(user.id);
+    logout(res);
+
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err: any) {
+    next(err);
+  }
+};
